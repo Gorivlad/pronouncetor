f30ed5dd1970ef6b5ce552b4109a240c0557f086
Gorivlad <peterprisc@gmail.com>
2024-07-21 21:52:11 +0200

    Implemented whisper, expanded diarization module

--- src/main.py
+++ src/main.py
@@ -1,22 +1,32 @@
-from pyannote.audio import Pipeline
-from pyannote.core import Segment
-from pyannote.audio import Audio
-import matplotlib.pyplot as plt
-from pyannote.audio.pipelines.utils.hook import ProgressHook
-from speaker_diarization import DiarizationModel
+from speaker_diarization import DiarizationModel, extract_and_save_clips
+import whisper
 
 def main():
     # Get the file path from user input
-    file_path = input("Insert filepath: ")
+    #file_path = input("Insert filepath: ")
+
+    # Temporary integrated file_path for testing
+    file_path = "/mnt/c/Personal/Coding/Study/audio_data/Callhome/audio_7.wav"
 
     # Initialize the diarization model
-    diarization_model = DiarizationModel()
+    diarization_object = DiarizationModel()
 
     # Perform diarization on the audio file
-    diarization_result = diarization_model.infer_file(file_path)
+    diarization_result = diarization_object.infer_file(file_path)
 
-    #Display results
-    diarization_model.display_results(diarization_result)
+    #Extract timestamps for target speaker
+    time_extraction = diarization_object.time_extraction_touples(diarization_result, "SPEAKER_01")
+
+    #Extract and save speaker segment audio
+    extract_and_save_clips(file_path, time_extraction)
+
+    #Transcribe one speaker and print text
+    model = whisper.load_model("base.en")
+    result = model.transcribe(audio="../data/processed/extracted.wav")
+    print(result["text"])
+
+
+
 
 if __name__ == "__main__":
     main()
--- src/speaker_diarization.py
+++ src/speaker_diarization.py
@@ -3,19 +3,19 @@ from pyannote.core import Segment
 from pyannote.audio import Audio
 import matplotlib.pyplot as plt
 from pyannote.audio.pipelines.utils.hook import ProgressHook
+from pydub import AudioSegment
 
 class DiarizationModel:
     def __init__(self, model_name="pyannote/speaker-diarization-3.0"):
         self.pipeline = Pipeline.from_pretrained(model_name)
 
-    def infer_file(self, file, num_speakers=2):
+    def infer_file(self, file_path, num_speakers=2):
+        # Diarization of audio
+        # Returns list of time-stamped segments e.g. [ 00:00:41.594 -->  00:00:44.226] AH SPEAKER_00
         with ProgressHook() as hook:
-            diarization_result = self.pipeline(file, hook=hook, num_speakers=num_speakers)
+            diarization_result = self.pipeline(file_path, hook=hook, num_speakers=num_speakers)
+        print("Diarization performed")
         return diarization_result
-
-    def display_results(self, diarization_result):
-        print("Diarization_results")
-        print(diarization_result)
 
     def plot_diarization(self, diarization_result, title="Diarization for the file"):
         fig, ax = plt.subplots(figsize=(10, 2))
@@ -26,3 +26,37 @@ class DiarizationModel:
         ax.set_xlabel("Time (s)")
         ax.set_title(title)
         plt.show()
+        print("Diarization plot displayed")
+
+    def time_extraction_list(self, diarization_result, target_speaker):
+        # Returns list of times [start,end,start,end...]
+        times = []
+        for turn, _, speaker in diarization_result.itertracks(yield_label=True):
+            if speaker == target_speaker:
+                times.append(turn.start)
+                times.append(turn.end)
+        print(f"Timestamps of {target_speaker} extracted as list")
+        return times
+
+    def time_extraction_touples(self, diarization_result, target_speaker):
+        # Returns list of touples [{start,end},{start,end}...]
+        times = []
+        for turn, _, speaker in diarization_result.itertracks(yield_label=True):
+            if speaker == target_speaker:
+                times.append((turn.start, turn.end))
+        print(f"Timestamps of {target_speaker} extracted as list of touples")
+        return times
+
+def extract_and_save_clips(file_path, time_segments, output_path = "../data/processed/extracted.wav"):
+        # Extract clips and save it as .wav file using pydub
+        # Time segments == list of touples
+        audio = AudioSegment.from_file(file_path)
+        extracted_audio = AudioSegment.empty()
+
+        for start, end in time_segments:
+            start_ms = start * 1000
+            end_ms = end * 1000 
+            extracted_audio += audio[start_ms:end_ms]
+
+        extracted_audio.export(output_path, format="wav")
+        print(f"Extracted audio saved to {output_path}")
